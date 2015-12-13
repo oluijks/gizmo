dff76cf3015a86a1c3f02968ef565b3c9418dac4
Olaf Luijks <oluijks@gmail.com>
2015-12-13 14:40:10 +0100

    Updated update command

--- src/Console/Commands/UpdateCommand.php
+++ src/Console/Commands/UpdateCommand.php
@@ -12,6 +12,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+error_reporting(1);
+
 /**
  * Updates Gizmo to the latest version
  *
